2e28ac7120c14aff5986eb6f5fbb80f3db4f26ee
박준하 <junha6316@gmail.com>
2025-04-21 10:51:30 +0900

    add: cacheProvider 싱글톤 구현 및 D1StorageProvider에 적용

--- apps/server/src/storage/factory.ts
+++ apps/server/src/storage/factory.ts
@@ -2,15 +2,24 @@ import type { Context } from "hono";
 import type { Env } from "../types/env";
 import { D1StorageProvider } from "./d1";
 import type { StorageProvider } from "./storage";
+import { InMemoryCacheProvider, type CacheProvider } from "./cache";
 
 let storageInstance: StorageProvider | null = null;
 let lastContext: Context<Env> | null = null;
+let cacheInstance: CacheProvider | null = null;
 
 export function getStorageProvider(ctx: Context<Env>): StorageProvider {
   // If context changed or no instance exists, create new instance
   if (!storageInstance || ctx !== lastContext) {
-    storageInstance = new D1StorageProvider(ctx);
+    storageInstance = new D1StorageProvider(ctx, getCacheProvider(ctx));
     lastContext = ctx;
   }
   return storageInstance;
+}
+
+export function getCacheProvider(ctx: Context<Env>): CacheProvider {
+  if (!cacheInstance) {
+    cacheInstance = new InMemoryCacheProvider();
+  }
+  return cacheInstance;
 }
