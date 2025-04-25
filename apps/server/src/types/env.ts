0967053fec2575ba26736f7971d1618a2789ce75
박준하 <junha6316@gmail.com>
2025-04-25 10:00:28 +0900

    수정: 환경 변수 수정 * S3_BUCKET_NAME -> AWS_S3_BUCKET_NAME

--- apps/server/src/types/env.ts
+++ apps/server/src/types/env.ts
@@ -15,6 +15,6 @@ export interface Env {
     AWS_REGION: string;
     AWS_ACCESS_KEY_ID: string;
     AWS_SECRET_ACCESS_KEY: string;
-    S3_BUCKET_NAME: string;
+    AWS_S3_BUCKET_NAME: string;
   };
 }
